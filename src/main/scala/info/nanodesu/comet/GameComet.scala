c7d529236be3fa39c55380b1b7b1e3dc3ec87145
Cola_Colin <cola_colin@ki.tng.de>
2013-12-30 11:42:35 +0100

    cache game id

--- src/main/scala/info/nanodesu/comet/GameComet.scala
+++ src/main/scala/info/nanodesu/comet/GameComet.scala
@@ -3,10 +3,20 @@ package info.nanodesu.comet
 import net.liftweb.http.CometActor
 import net.liftweb.util.Helpers
 import net.liftweb.http.CometListener
+import net.liftweb.common.Full
+import net.liftweb.common.Box
 
 abstract class GameComet extends CometActor with CometListener {
   def nameKey: String
-  def getGameId = for (n <- name; gId <- Helpers.tryo(n.replaceAll(nameKey, "").toInt)) yield gId
-  def isMyGame(id: Int) = id == getGameId.openOr(-1)
+  protected var cachedGameId: Box[Int] = null
+  def getGameId = { 
+    if (cachedGameId == null) {
+      cachedGameId = for (n <- name; gId <- Helpers.tryo(n.replaceAll(nameKey, "").toInt)) yield gId
+    }
+    cachedGameId
+  }
+  def isMyGame(id: Int) = {
+    id == getGameId.getOrElse(-1)
+  }
   def registerWith = GameCometServer
 }
