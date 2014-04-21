9662b833b4cb3f1db85b735bb1266a39d6ec4110
Frederik Eychenie <feychenie@gmail.com>
2014-04-21 23:30:42 +0200

    db initialization script fix

--- db_definition/tablesv2.sql
+++ db_definition/tablesv2.sql
@@ -5,24 +5,18 @@ DROP TABLE IF EXISTS v2_display_name_history  CASCADE;
 DROP TABLE IF EXISTS v2_player CASCADE;
 DROP TABLE IF EXISTS v2_player_game_rel  CASCADE;
 DROP TABLE IF EXISTS v2_timepoint_stats CASCADE;
-DROP TABLE IF EXISTS v2_planet CASCADE;
+DROP TABLE IF EXISTS v2_planet_json CASCADE;
+DROP TABLE IF EXISTS v2_teams CASCADE;
+DROP TABLE IF EXISTS v2_army_events CASCADE;
+DROP TABLE IF EXISTS v2_spec_keys CASCADE;
+
+DROP SEQUENCE IF EXISTS team_id;
 
 CREATE TABLE v2_settings (
 	report_version integer
 );
 
 INSERT INTO v2_settings (report_version) VALUES (17);
-
-CREATE TABLE v2_planet (
-	id serial PRIMARY KEY,
-	biome varchar NOT NULL,
-	height_range numeric NOT NULL,
-	planet_name varchar NOT NULL,
-	radius numeric NOT NULL,
-	seed integer NOT NULL,
-	temperature numeric NOT NULL,
-	water_height numeric NOT NULL
-);
 
 CREATE TABLE v2_planet_json (
 	id serial PRIMARY KEY,
@@ -58,6 +52,13 @@ CREATE TABLE v2_display_name_history (
 	replaced_on timestamp NOT NULL
 );
 
+CREATE TABLE v2_teams (
+	id integer PRIMARY KEY,
+	ingame_id integer default 0 NOT NULL,
+	primary_color varchar,
+	secondary_color varchar
+);
+
 CREATE SEQUENCE team_id start 1;
 
 CREATE TABLE v2_player_game_rel (
@@ -68,13 +69,6 @@ CREATE TABLE v2_player_game_rel (
 	locked boolean default 'false' NOT NULL,
 	died boolean default 'false' NOT NULL,
 	UNIQUE(g, p)
-);
-
-CREATE TABLE v2_teams (
-	id integer PRIMARY KEY,
-	ingame_id integer default 0 NOT NULL,
-	primary_color varchar,
-	secondary_color varchar
 );
    
 CREATE TABLE v2_timepoint_stats (
