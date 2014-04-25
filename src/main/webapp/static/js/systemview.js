5f5488e6f9329f59b450fe9f8046f2392886fd35
Cola_Colin <cola_colin@ki.tng.de>
2014-04-25 21:06:59 +0200

    place some files in the right place for production add a help link to the start replay link

---
+++ src/main/webapp/static/js/systemview.js
@@ -0,0 +1,17 @@
+  $(document).ready(function(){
+    var gameId = $('#systemInfo').data('gameid');
+    $.getJSON('/report/getsystem?gameId=' + gameId, function(data){
+      function SystemModel(){
+        this.name = data.name
+        this.align = data.planets.length > 1 ? "left":"center";
+        this.Planets = data.planets; //_.sortBy(data.planets, function(p){return p.planet.radius * -1});
+        _.map( this.Planets, function(planet){
+          planet.imagePath = imageBaseUrl + 'planets/' + planet.planet.biome + '.png';
+          planet.imageWidth = planet.planet.radius >= 1000 ? '200px' : Math.round(200 * planet.planet.radius / 1000) + 'px';
+          planet.imageOffset = planet.planet.radius < 420 ? Math.round(200 * (420 - planet.planet.radius)/2000) + 'px': '0';
+          planet.starting_planet = planet.starting_planet || false; //ensure field presence
+        });
+      }
+      ko.applyBindings( new SystemModel(), document.getElementById('systemInfo'))
+    })
+  })
