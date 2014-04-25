5e3d1a9ccecebeb90ba210fa903502d4196b3cd6
Frederik Eychenie <feychenie@gmail.com>
2014-04-25 22:33:55 +0200

    support planets with radius 0

--- src/main/webapp/static/js/systemview.js
+++ src/main/webapp/static/js/systemview.js
@@ -6,11 +6,14 @@
         this.align = data.planets.length > 1 ? "left":"center";
         this.Planets = data.planets; //_.sortBy(data.planets, function(p){return p.planet.radius * -1});
         _.map( this.Planets, function(planet){
+          //--compat fixes
+          if(planet.planet.radius <= 0 ) planet.planet.radius = 500;
+          planet.starting_planet = planet.starting_planet || false; //ensure field presence
+          planet.required_thrust_to_move = planet.required_thrust_to_move || 0; //ensure field presence
+          // --end compat fixes
           planet.imagePath = imageBaseUrl + 'planets/' + planet.planet.biome + '.png';
           planet.imageWidth = planet.planet.radius >= 1000 ? '200px' : Math.round(200 * planet.planet.radius / 1000) + 'px';
           planet.imageOffset = planet.planet.radius < 420 ? Math.round(200 * (420 - planet.planet.radius)/2000) + 'px': '0';
-          planet.starting_planet = planet.starting_planet || false; //ensure field presence
-          planet.required_thrust_to_move = planet.required_thrust_to_move || 0; //ensure field presence
         });
       }
       ko.applyBindings( new SystemModel(), document.getElementById('systemInfo'))
