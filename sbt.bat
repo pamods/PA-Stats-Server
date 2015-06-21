230c1facab8faecee0f764767745745dbfae8c93
ColaColin <cola_colin@ki.tng.de>
2015-06-21 15:39:02 +0200

    make sbt.bat use jdk 7

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,4 @@
+setlocal
+set PATH=C:\Program Files\Java\jdk7\bin;%PATH%
 set SCRIPT_DIR=%~dp0
 java -Drun.mode=development -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024m -Xmx400M -Xss2M -jar "%SCRIPT_DIR%\sbt-launch-0.12.1.jar" %*
